c7a72d5a3bbbb2876e89e395a54a2b2d17cbc3f1
Suraj-H <suraj.14121990@gmail.com>
2022-04-08 14:42:30 +0530

    test-added dto

---
+++ src/game/dtos/create-game.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+import { GameType } from '../types/game-type.enum';
+import { PieceCode } from '../types/piece-code.enum';
+
+export class CreateGameDto {
+  @IsNotEmpty()
+  gameType: GameType;
+
+  @IsNotEmpty()
+  userOnePieceCode: PieceCode;
+}
