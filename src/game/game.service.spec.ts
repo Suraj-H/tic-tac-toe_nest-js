8d1ae0e1839ce8eee0eaab85fd843a71d45e7ee1
Suraj-H <suraj.14121990@gmail.com>
2022-04-09 22:50:10 +0530

    added - getGame/getGamesToJoin test cases

--- src/game/game.service.spec.ts
+++ src/game/game.service.spec.ts
@@ -8,7 +8,31 @@ import { GameService } from './game.service';
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 const createMockRepository = <T = any>(): MockRepository<T> => ({
   findOne: jest.fn(),
+  find: jest.fn(),
+  createQueryBuilder: jest.fn(),
 });
+
+const mockUser = {
+  id: 1,
+  username: 'test',
+  email: 'test@gamil.com',
+  password: 'test',
+  games: [],
+  moves: [],
+};
+
+const mockJoinGame = {
+  id: 1,
+  slug: 'test',
+  userOne: mockUser,
+  userTwo: null,
+  gameStatus: 'WAITS_FOR_USER',
+  gameType: 'COMPETITION',
+  userOnePieceCode: 'O',
+  createdAt: '2020-01-01T00:00:00.000Z',
+  endedAt: '2020-01-01T00:00:00.000Z',
+  moves: [],
+};
 
 describe('GameService', () => {
   let service: GameService;
@@ -37,7 +61,7 @@ describe('GameService', () => {
         const gameId = 1;
         const expectedGame = {};
 
-        gameRepository.findOne.mockReturnValue(expectedGame);
+        gameRepository.findOne.mockResolvedValue(expectedGame);
         const game = await service.getGame(gameId);
         expect(game).toEqual(expectedGame);
       });
@@ -46,6 +70,7 @@ describe('GameService', () => {
     describe('when game with ID does not exist', () => {
       it('should throw the NotFoundException', async () => {
         const gameId = 1;
+
         gameRepository.findOne.mockReturnValue(undefined);
 
         try {
@@ -53,6 +78,41 @@ describe('GameService', () => {
         } catch (error) {
           expect(error).toBeInstanceOf(NotFoundException);
           expect(error.message).toEqual(`Game with id #${gameId} not found.`);
+        }
+      });
+    });
+  });
+
+  describe('getGamesToJoin', () => {
+    describe('when there are games to join', () => {
+      it('should return games', async () => {
+        const expectedGames = [];
+
+        gameRepository.find.mockResolvedValue(expectedGames);
+
+        const games = await service.getGamesToJoin(mockUser);
+        expect(games).toEqual(expectedGames);
+      });
+    });
+
+    describe('when there are no games to join', () => {
+      it('should return empty array', async () => {
+        gameRepository.find.mockResolvedValue([]);
+
+        const games = await service.getGamesToJoin(mockUser);
+        expect(games).toEqual([]);
+      });
+    });
+
+    describe('when current user does not exist', () => {
+      it('should throw the NotFoundException', async () => {
+        const user = undefined;
+
+        try {
+          expect(user).toEqual(undefined);
+        } catch (error) {
+          expect(error).toBeInstanceOf(NotFoundException);
+          expect(error.message).toEqual('User not found.');
         }
       });
     });
